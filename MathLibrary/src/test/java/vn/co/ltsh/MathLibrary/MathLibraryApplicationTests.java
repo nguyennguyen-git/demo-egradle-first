54cf5d9bed9536600ed411bd5a14d897e270b81c
NguyenHo <nmnguyen1411.git@gmail.com>
2020-10-20 16:38:45 +0700

    Commit first

---
+++ MathLibrary/src/test/java/vn/co/ltsh/MathLibrary/MathLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.co.ltsh.MathLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MathLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
